e28fad3186b4f55aa67e78fc7ebe5817d5aafe68
Achraf Ben Soltane <achrafb.s2015@gmail.com>
2021-02-18 19:47:15 +0100

    first commit

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Item;
+use Carbon\Carbon;
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Item::orderBy('created_at', 'Desc')->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $newItem = new Item();
+        $newItem->name = $request->item['name'];
+        $newItem->save();
+        return $newItem;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $item = Item::find($id);
+        if($item){
+            $item->completed = $request->item['completed'];
+            $item->completed_at = $request->item['completed'] ? Carbon::now()->setTimezone('Africa/Tunis') : null;
+            $item->save();
+            return $item;
+        }
+        return "Item not found";    
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $item = Item::find($id);
+        if($item){
+            $item->delete();
+            return "Item deleted Successfully";
+        }
+        return "Item not found";
+    }
+}
---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+require('./bootstrap');
+import Vue from 'vue';
+
+import App from './vue/app.vue';
+
+const app = new Vue({
+    el: '#app',
+    components: { App }
+});
